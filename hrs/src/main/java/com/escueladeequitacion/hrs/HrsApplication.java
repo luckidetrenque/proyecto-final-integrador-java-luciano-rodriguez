d104a08e15336188c1b467b5828dfa2d34312675
Tati <santiagomatheu@gmail.com>
2026-02-28 16:44:11 -0300

    Configurado el acceso a BBDD Neon

--- hrs/src/main/java/com/escueladeequitacion/hrs/HrsApplication.java
+++ hrs/src/main/java/com/escueladeequitacion/hrs/HrsApplication.java
@@ -1,7 +1,7 @@
 package com.escueladeequitacion.hrs;
 
 import java.util.TimeZone;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,6 +11,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class HrsApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		TimeZone.setDefault(TimeZone.getTimeZone("America/Argentina/Buenos_Aires"));
 		SpringApplication.run(HrsApplication.class, args);
 	}
